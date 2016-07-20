cd1d7483a761749d807890752e5b8bff368114ea
jkillz2020 <kilgore.josh@gmail.com>
2016-07-19 21:08:23 -0500

    finished first 2 ex

---
+++ for-loops.js
@@ -0,0 +1,16 @@
+
+
+// for (var number =5; number <= 130; number = number + 10) {
+
+// console.log("Current value is", number);
+// }
+
+// for (var number2 = 4096; number2 >= 1; number2 = number2 / 2) {
+//   console.log("Current value is", number2);
+// }
+
+var presidents = ["George Washington", "John Adams", "Thomas Jefferson"];
+
+for (var presidents; presidents.length.i; presidents++) {
+  console.log("President # + i", presidents)
+}
